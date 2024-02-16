bbaaf313b3e122263c217dc409ed121b5cda3845
NishantRana07 <126577697+NishantRana07@users.noreply.github.com>
2024-02-16 23:00:21 +0530

    Add files via upload

---
+++ countries.js
@@ -0,0 +1,13 @@
+const countries = {
+  "en":"English",
+  "hi": "Hindi",
+  "bn": "Bengali",
+  "te": "Telugu",
+  "mr": "Marathi",
+  "ta": "Tamil",
+  "ur": "Urdu",
+  "gu": "Gujarati",
+  "kn": "Kannada",
+  "or": "Odia",
+  "ml": "Malayalam"
+}
